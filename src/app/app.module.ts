4dfe52c39b157a94eb5a7267d466693448a6c08e
Joe <joe.bell.1329@gmail.com>
2020-08-29 19:25:28 +0100

    add runtime config service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConfigService } from './config/config.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: (config: ConfigService) => () => config.loadConfig().toPromise(),
+      deps: [ ConfigService ],
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/config/config.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { shareReplay } from 'rxjs/operators';
+
+type Config = {
+  auth: {
+    client_id: string
+  };
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConfigService {
+
+  public config$: Observable<Config>;
+
+  constructor(private http: HttpClient) { }
+
+  public loadConfig(): Observable<Config> {
+    if (!this.config$) {
+      this.config$ = this.http.get<Config>('/assets/config.json').pipe(shareReplay(1));
+    }
+
+    return this.config$;
+  }
+}

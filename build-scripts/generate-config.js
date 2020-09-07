ba2d21a574a6efa7b14f2615e29c24637a6a58b9
Joe <joe.bell.1329@gmail.com>
2020-09-07 23:15:31 +0100

    remove unused config vars

--- build-scripts/generate-config.js
+++ build-scripts/generate-config.js
@@ -4,7 +4,5 @@ const config = JSON.parse(fs.readFileSync('./src/assets/config.sample.json', 'ut
 
 config.auth.client_id = process.env.AUTH0_CLIENT_ID;
 config.auth.domain = process.env.AUTH0_DOMAIN;
-config.auth.audience = process.env.AUTH0_AUDIENCE;
-config.auth.login_callback = process.env.AUTH0_CALLBACK;
 
 fs.writeFileSync('./dist/prepit-planner/assets/config.json', JSON.stringify(config), 'utf-8');

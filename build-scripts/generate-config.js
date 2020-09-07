ff5743cf93f7446eb357a33526fda68c441f4e09
Joe <joe.bell.1329@gmail.com>
2020-09-07 23:08:52 +0100

    fix path in build script

--- build-scripts/generate-config.js
+++ build-scripts/generate-config.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 
-const config = JSON.parse(fs.readFileSync('/src/assets/config.sample.json', 'utf-8'));
+const config = JSON.parse(fs.readFileSync('./src/assets/config.sample.json', 'utf-8'));
 
 config.auth.client_id = process.env.AUTH0_CLIENT_ID;
 config.auth.domain = process.env.AUTH0_DOMAIN;
 config.auth.audience = process.env.AUTH0_AUDIENCE;
 config.auth.login_callback = process.env.AUTH0_CALLBACK;
 
-fs.writeFileSync('/dist/prepit-planner/assets/config.json', JSON.stringify(config), 'utf-8');
+fs.writeFileSync('./dist/prepit-planner/assets/config.json', JSON.stringify(config), 'utf-8');
